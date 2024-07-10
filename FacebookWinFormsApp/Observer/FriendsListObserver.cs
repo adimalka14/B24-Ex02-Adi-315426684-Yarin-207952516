b375707da10c2881ce560918b4aeaba80a236d3e
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-07-10 19:33:08 +0300

    fix coding standart

--- FacebookWinFormsApp/Observer/FriendsListObserver.cs
+++ FacebookWinFormsApp/Observer/FriendsListObserver.cs
@@ -1,9 +1,5 @@
 ﻿using FacebookWrapper.ObjectModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BasicFacebookFeatures.Observer
