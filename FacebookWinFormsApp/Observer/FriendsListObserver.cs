e29d408127c14cd5ab76aa0a6e7337d424683c94
Yarin Yahav <yarinyahav0@gmail.com>
2024-07-10 17:03:12 +0300

    addind the observer design pattern

---
+++ FacebookWinFormsApp/Observer/FriendsListObserver.cs
@@ -0,0 +1,37 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures.Observer
+{
+    public class FriendsListObserver : IObserver
+    {
+        private ListBox listBoxFriends;
+        private PictureBox pictureBoxFriend;
+
+        public FriendsListObserver(ListBox i_ListBoxFriends, PictureBox i_PictureBoxFriend)
+        {
+            listBoxFriends = i_ListBoxFriends;
+            pictureBoxFriend = i_PictureBoxFriend;
+        }
+
+        public void Update(User user)
+        {
+            var friends = user.Friends;
+
+            listBoxFriends.Invoke(new Action(() =>
+            {
+                listBoxFriends.Items.Clear();
+                listBoxFriends.DisplayMember = "Name";
+                foreach (var friend in friends)
+                {
+                    listBoxFriends.Items.Add(friend);
+                }
+            }));
+        }
+    }
+}
---
+++ FacebookWinFormsApp/Observer/IObserver.cs
@@ -0,0 +1,14 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures.Observer
+{
+    public interface IObserver
+    {
+        void Update(User user);
+    }
+}
